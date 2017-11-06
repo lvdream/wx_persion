3116a53353261ac313a790331382fa70b30b17e7
lvdream <lvdream@gmail.com>
2017-11-06 21:32:32 +0800

    test

--- src/main/java/com/github/binarywang/demo/spring/handler/MsgHandler.java
+++ src/main/java/com/github/binarywang/demo/spring/handler/MsgHandler.java
@@ -1,18 +1,17 @@
 package com.github.binarywang.demo.spring.handler;
 
-import java.util.Map;
-
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Component;
-
 import com.github.binarywang.demo.spring.builder.TextBuilder;
 import com.github.binarywang.demo.spring.service.WeixinService;
-
 import me.chanjar.weixin.common.api.WxConsts;
+import me.chanjar.weixin.common.session.WxSession;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * 
@@ -28,7 +27,7 @@ public class MsgHandler extends AbstractHandler {
             WxSessionManager sessionManager)    {
 
         WeixinService weixinService = (WeixinService) wxMpService;
-
+        logger.info(wxMessage.getContent());
         if (!wxMessage.getMsgType().equals(WxConsts.XML_MSG_EVENT)) {
             //TODO 可以选择将消息保存到本地
         }
@@ -40,7 +39,9 @@ public class MsgHandler extends AbstractHandler {
                 .TRANSFER_CUSTOMER_SERVICE().fromUser(wxMessage.getToUser())
                 .toUser(wxMessage.getFromUser()).build();
         }
-
+        WxSession session =  sessionManager.getSession(wxMessage.getFromUser());
+//        session.setAttribute();
+//        this.logger.info(sessionManager.getSession(wxMessage.getFromUser()));
         //TODO 组装回复消息
         String content = "回复信息内容";
         return new TextBuilder().build(content, wxMessage, weixinService);
